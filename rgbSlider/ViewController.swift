477648c9d7f390b74e4a44dfdbff6370b725935a
Sokol8807 <sokol8807@gmail.com>
2022-08-06 16:49:23 +0300

    Initial Commit

---
+++ rgbSlider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rgbSlider
+//
+//  Created by Ilya Sokolov on 06.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
