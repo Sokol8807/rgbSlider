a76f2df807621cabb747fe52c97ba408965fb262
Sokol8807 <sokol8807@gmail.com>
2022-08-07 11:44:50 +0300

    внес финальные правки

--- rgbSlider/ViewController.swift
+++ rgbSlider/ViewController.swift
@@ -35,28 +35,43 @@ final class ViewController: UIViewController {
         colorView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
         redValueLabel.text = String(redSlider.value)
         redValueLabel.text = String(round(redSlider.value * 100) / 100)
+        setupColorSliderThumb()
     }
     
     @IBAction func moveGreenSlider() {
         green = CGFloat(greenSlider.value)
         colorView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
         greenValueLabel.text = String(round(greenSlider.value * 100) / 100)
+        setupColorSliderThumb()
     }
- 
+    
     @IBAction func moveBlueSlider() {
         blue = CGFloat(blueSlider.value)
         colorView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
         blueValueLabel.text = String(blueSlider.value)
         blueValueLabel.text = String(round(blueSlider.value * 100) / 100)
+        setupColorSliderThumb()
     }
     
-   // MARK: - Privar Methods
+    // MARK: - Privar Methods
     private func layoutSetup() {
-        colorView.layer.cornerRadius = 10
+        view.backgroundColor = .systemCyan
+        colorView.layer.cornerRadius = colorView.frame.width / 20
         redValueLabel.text = "0.00"
+        redValueLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
         greenValueLabel.text = "0.00"
+        greenValueLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
         blueValueLabel.text = "0.00"
+        blueValueLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     }
-
+    
+    private func setupColorSliderThumb () {
+        redSlider.thumbTintColor = colorView.backgroundColor
+        greenSlider.thumbTintColor = colorView.backgroundColor
+        blueSlider.thumbTintColor = colorView.backgroundColor
+    }
+    
 }
 
